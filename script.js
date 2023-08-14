4f62a790ae0d06fd99df37f0d15865de6bf9f21a
shwetasharma1302 <126080003+shwetasharma1302@users.noreply.github.com>
2023-08-14 10:32:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const players = ["John", "Bob", "Alice", "Poppy"];
-
 const person = {
   name: "John Doe",
   age: 80,
 };
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+// Create a reference to the 'players' array
+const team = players;
 
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
+// Create a copy of the 'players' array
+const team1 = players.slice();
+
+// Create a copy of the 'person' object
+const cap1 = Object.assign({}, person);
+
+// Modifying the original 'players' array won't affect 'team1'
+players.push("Sarah");
+
+// Modifying the original 'person' object won't affect 'cap1'
+person.name = "Jane";
+
+console.log("Original Players:", players);
+console.log("Team:", team);
+console.log("Team 1:", team1);
+console.log("Original Person:", person);
+console.log("Captain 1:", cap1);
